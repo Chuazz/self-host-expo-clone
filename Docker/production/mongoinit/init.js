18687b4a6c9f0574879eab9a0c323c711a33e42e
sonnv <sonnv1912@gmail.com>
2024-10-02 03:51:46 +0000

    chore: commit to change OS

--- Docker/production/mongoinit/init.js
+++ Docker/production/mongoinit/init.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-undef */
-let db = connect('mongodb://admin:myPRODUCTIONPassword@localhost:27017/admin')
+try {
+  // let db = connect(
+  //   "mongodb://admin:myPRODUCTIONPassword@localhost:27017/admin"
+  // );
 
-db = db.getSiblingDB('expo')
+  // db = db.getSiblingDB("expo");
 
-db.createUser({
-  user: 'expo',
-  pwd: 'myRegularUserPassw',
-  roles: [
-    {
-      role: 'readWrite',
-      db: 'expo'
-    }
-  ]
-})
+  db.createUser({
+    user: "expo",
+    pwd: "myRegularUserPassw",
+    roles: [
+      {
+        role: "readWrite",
+        db: "expo",
+      },
+    ],
+  });
+} catch (error) {
+  console.log("🚀 ~ error:", error);
+}
